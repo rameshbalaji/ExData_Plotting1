1c0f06be272b76a6d27de0020b363064ee6467e7
rameshbalaji <Yudhishtra.r@gmail.com>
2016-01-10 21:24:12 +0530

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,20 @@
+## Setting the working directory
+
+## setwd("C:/Ramesh/R Programs/Exploratory Data/")
+## Get the power consumption data
+getallpowerdata <- read.table(file="household_power_consumption.txt", 
+header=T, sep=";")
+
+getfebpowerdatafor2days <- getallpowerdata[getallpowerdata$Date %in% c("1/2/2007","2/2/2007") ,]
+
+combinedateandtime <- strptime(paste(getfebpowerdatafor2days$Date, getfebpowerdatafor2days$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+
+getfebpowerdatafor2days$Global_active_power <- as.numeric(getfebpowerdatafor2days$Global_active_power)
+
+
+png(filename = "Plot2.png",
+    width = 480, height = 480, units = "px")
+
+plot(combinedateandtime, getfebpowerdatafor2days$Global_active_power , xlab = "", ylab = "Global Active Power (kilowatts)", type="l")
+
+dev.off()
